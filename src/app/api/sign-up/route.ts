ea47cb77e5c8aafdffb71d15bf3b96026de55522
crazyhaller <suvigyamadrid@gmail.com>
2024-04-21 23:57:21 +0530

    Finish: SignUp Route & Logic

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -8,6 +8,96 @@ export async function POST(request: Request) {
 
   try {
     const { username, email, password } = await request.json()
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    })
+
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: 'Username already exists',
+        },
+        {
+          status: 400,
+        }
+      )
+    }
+
+    const existingUserByEmail = await UserModel.findOne({
+      email,
+    })
+
+    const verificationCode = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString()
+
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: 'Email already exists',
+          },
+          {
+            status: 400,
+          }
+        )
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        existingUserByEmail.password = hashedPassword
+        existingUserByEmail.verifyCode = verificationCode
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+        await existingUserByEmail.save()
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10)
+
+      const expiryDate = new Date()
+      expiryDate.setHours(expiryDate.getHours() + 1)
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode: verificationCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      })
+
+      await newUser.save()
+    }
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verificationCode
+    )
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        {
+          status: 500,
+        }
+      )
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: 'User registered! Verification email sent',
+      },
+      {
+        status: 201,
+      }
+    )
   } catch (error) {
     console.error('Error signing up: ', error)
     return Response.json(
